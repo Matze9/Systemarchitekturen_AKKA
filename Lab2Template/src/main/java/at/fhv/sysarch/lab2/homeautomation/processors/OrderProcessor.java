c9fc1e0ceda32b27ccdb216ee0368b7e2c1f0d98
Richi <clo9450@students.fhv.at>
2021-11-23 16:02:48 +0100

    experimented with communication between actors

--- Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/processors/OrderProcessor.java
+++ Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/processors/OrderProcessor.java
@@ -1,4 +1,87 @@
 package at.fhv.sysarch.lab2.homeautomation.processors;
 
-public class OrderProcessor {
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+import at.fhv.sysarch.lab2.homeautomation.devices.Fridge;
+import at.fhv.sysarch.lab2.homeautomation.devices.sensors.FridgeSpaceSensor;
+import at.fhv.sysarch.lab2.homeautomation.modelClasses.Product;
+import at.fhv.sysarch.lab2.homeautomation.modelClasses.Stock;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OrderProcessor extends AbstractBehavior<OrderProcessor.OrderProcessorCommand> {
+
+
+
+    public interface OrderProcessorCommand{}
+
+    public static class SpaceValidationResponse implements OrderProcessorCommand{
+        private String msg;
+        private ActorRef<FridgeSpaceSensor.ValidateSpace> from;
+
+        public SpaceValidationResponse(String msg, ActorRef<FridgeSpaceSensor.ValidateSpace> from){
+            this.msg = msg;
+            this.from = from;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public ActorRef<FridgeSpaceSensor.ValidateSpace> getFrom() {
+            return from;
+        }
+    }
+
+    public static class CallFridgeSpaceSensor implements OrderProcessorCommand{
+        private LinkedList<Product> products;
+        public final ActorRef<FridgeSpaceSensor.ValidateSpace> spaceSensor;
+
+        public CallFridgeSpaceSensor(LinkedList<Product> products, ActorRef<FridgeSpaceSensor.ValidateSpace> spaceSensor){
+            this.products= products;
+            this.spaceSensor = spaceSensor;
+        }
+    }
+
+
+
+
+    public static Behavior<OrderProcessorCommand>create(ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessorCommand){
+        return Behaviors.setup(context -> new OrderProcessor(context, orderProcessorCommand));
+    }
+
+    private static Stock stock;
+    private final ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessorCommand;
+
+    public OrderProcessor(ActorContext<OrderProcessorCommand>context, ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessorCommand){
+        super(context);
+        stock = new Stock();
+        this.orderProcessorCommand = orderProcessorCommand;
+
+        getContext().getLog().info("OrderProcessor activated");
+
+    }
+
+    private Behavior<OrderProcessorCommand> onFridgeSpaceSensorCall(CallFridgeSpaceSensor c){
+        c.spaceSensor.tell(new FridgeSpaceSensor.ValidateSpace(c.products, stock, getContext().getSelf()));
+        return Behaviors.same();
+    }
+
+    private Behavior<OrderProcessorCommand> onFridgeSpaceSensorResponse(SpaceValidationResponse s){
+        getContext().getLog().info("[ORDERPROCESSOR] reponse recieved from space sensor: " + s.getMsg());
+        return this;
+    }
+
+
+    @Override
+    public Receive<OrderProcessorCommand> createReceive() {
+        return newReceiveBuilder().onMessage(CallFridgeSpaceSensor.class, this::onFridgeSpaceSensorCall).onMessage(SpaceValidationResponse.class, this::onFridgeSpaceSensorResponse).build();
+    }
+
+
 }
--- Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
+++ Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
@@ -12,10 +12,12 @@ import at.fhv.sysarch.lab2.homeautomation.HomeAutomationController;
 import at.fhv.sysarch.lab2.homeautomation.devices.AirCondition;
 import at.fhv.sysarch.lab2.homeautomation.devices.Fridge;
 import at.fhv.sysarch.lab2.homeautomation.devices.TemperatureSensor;
+import at.fhv.sysarch.lab2.homeautomation.devices.sensors.FridgeSpaceSensor;
 import at.fhv.sysarch.lab2.homeautomation.modelClasses.Banana;
 import at.fhv.sysarch.lab2.homeautomation.modelClasses.Butter;
 import at.fhv.sysarch.lab2.homeautomation.modelClasses.Milk;
 import at.fhv.sysarch.lab2.homeautomation.modelClasses.Product;
+import at.fhv.sysarch.lab2.homeautomation.processors.OrderProcessor;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -27,18 +29,22 @@ public class UI extends AbstractBehavior<Void> {
     private ActorRef<TemperatureSensor.TemperatureCommand> tempSensor;
     private ActorRef<AirCondition.AirConditionCommand> airCondition;
     private ActorRef<Fridge.FridgeCommand> fridge;
+    private ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessor;
+    private ActorRef<FridgeSpaceSensor.ValidateSpace> spaceSensor;
 
-    public static Behavior<Void> create(ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition, ActorRef<Fridge.FridgeCommand> fridge) {
-        return Behaviors.setup(context -> new UI(context, tempSensor, airCondition, fridge));
+    public static Behavior<Void> create(ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition, ActorRef<Fridge.FridgeCommand> fridge, ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessor, ActorRef<FridgeSpaceSensor.ValidateSpace> spaceSensor) {
+        return Behaviors.setup(context -> new UI(context, tempSensor, airCondition, fridge, orderProcessor, spaceSensor));
     }
 
-    private  UI(ActorContext<Void> context, ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition, ActorRef<Fridge.FridgeCommand> fridge) {
+    private  UI(ActorContext<Void> context, ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition, ActorRef<Fridge.FridgeCommand> fridge, ActorRef<OrderProcessor.OrderProcessorCommand> orderProcessor, ActorRef<FridgeSpaceSensor.ValidateSpace> spaceSensor) {
         super(context);
         // TODO: implement actor and behavior as needed
         // TODO: move UI initialization to appropriate place
         this.airCondition = airCondition;
         this.tempSensor = tempSensor;
         this.fridge = fridge;
+        this.orderProcessor = orderProcessor;
+        this.spaceSensor = spaceSensor;
         new Thread(() -> { this.runCommandLine(); }).start();
 
         getContext().getLog().info("UI started");
@@ -102,6 +108,23 @@ public class UI extends AbstractBehavior<Void> {
                     this.fridge.tell(new Fridge.ConsumeProduct(p));
 
                 }
+            }
+            if(command[0].equals("o")){
+                Product p = null;
+
+                if(command[1].equals("a")){
+                    p = new Butter();
+                }
+                if(command[1].equals("b")){
+                    p = new Milk();
+                }
+                if(command[1].equals("c")){
+                    p = new Banana();
+                }
+                LinkedList<Product> list = new LinkedList<>();
+                list.add(p);
+                this.orderProcessor.tell(new OrderProcessor.CallFridgeSpaceSensor(list, spaceSensor ));
+
             }
 
             // TODO: process Input
